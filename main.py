d807f30e09f1ffcb0b0f4317970e6bd6b1244d5b
Dougie Peart <admin@dougiepeart.co.uk>
2025-08-05 20:56:32 +0100

    Dirty dirty char counter

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     num_words = get_num_words(text)
     num_chars = get_num_chars(text)
     print(f"{num_words} words found in the document")
-    print(f"{num_chars} chars found in the document")
+    print(num_chars)
 
 
 def get_book_text(path):
--- stats.py
+++ stats.py
@@ -4,6 +4,20 @@ def get_num_words(contents):
 
 
 def get_num_chars(contents):
-    words = list(contents)
-    chars = list(words)
-    return chars.count()
+    lower = contents.lower()
+    chars = list(lower)
+    counts = dict()
+    seen = set()
+    duplicates = []
+    char_list = []
+    for char in chars:
+        char_list.append(char)
+    for char in char_list:
+        if char in seen:
+            duplicates.append(char)
+        else:
+            seen.add(char)
+    for char in duplicates:
+        if char in seen:
+            counts[char] = counts.get(char, 1) + 1
+    return counts
