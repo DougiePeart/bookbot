5cdfc890a575a0f163d3f54e8b07a91ad89cbcde
Dougie Peart <admin@dougiepeart.co.uk>
2025-09-01 11:21:46 +0100

    Finished

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
+import sys
 from stats import get_num_words
 from stats import get_num_chars
-from stats import get_chars_dict
+from stats import sort_chars
 
 
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
-    num_words = get_num_words(text)
-    num_chars = get_num_chars(text)
-    chars = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(num_chars)
-#    print(chars)
-
+    if len(sys.argv) > 1:
+        book_path = sys.argv[1]
+        text = get_book_text(book_path)
+        num_words = get_num_words(text)
+        num_chars = get_num_chars(text)
+        sorted_chars = sort_chars(num_chars)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {book_path}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("----------- Character Count ----------")
+        for item in sorted_chars:
+            print(f"{item['char']}: {item['num']}")
+    else:
+        sys.exit("Usage: python3 main.py <path_to_book>")
 
 def get_book_text(path):
     with open(path) as f:
--- stats.py
+++ stats.py
@@ -8,13 +8,16 @@ def get_num_chars(text):
     chars = {}
     for char in lower:
         chars[char] = chars.get(char, 0) + 1
-
-def get_chars_dict(text):
-    chars = {}
-    for c in text:
-        lowered = c.lower()
-        if lowered in chars:
-            chars[lowered] += 1
-        else:
-            chars[lowered] = 1
     return chars
+
+def sort_on(items):
+    return items["num"]
+
+def sort_chars(num_chars):
+    list_of_dicts = [
+        {"char": char, "num": count} 
+        for char, count in num_chars.items()
+        if char.isalpha()
+    ]
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
